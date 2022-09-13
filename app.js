e40bb39ffc546a123042720aa2a66dbb288e177a
MehmetFK <109756623+HaciGustav@users.noreply.github.com>
2022-09-13 15:01:15 +0200

    .

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const randomNumber = () =>{
             // document.body.style.backgroundColor = hexColor
             console.log(hexColor)
             item.style.background = hexColor
-            hexValue.innerText = hexColor
+            //hexValue.innerText = hexColor
         })
 
     } )
